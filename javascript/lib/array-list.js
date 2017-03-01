fb54fb5f835318729129fae9797bdfb6e15cba6a
Ali Zand <azand@on-site.com>
2017-03-01 14:08:54 -0800

    02 Up to prepend

--- javascript/lib/array-list.js
+++ javascript/lib/array-list.js
@@ -20,10 +20,44 @@ ArrayList.prototype.get = function(index) {
 // Define a method "add" which takes a single argument. This method should
 // append the argument to the end of this ArrayList and increase the size by
 // 1. The return value must be this.
+ArrayList.prototype.add = function(value) {
+  this._expand();
+  this._array.set(this._size, value);
+  this._size++;
+  return this;
+};
 
 // Define a method "prepend" which takes a single argument. This method should
 // prepend the argument to the beginning of this ArrayList and increase the size
 // by 1. The return value must be this.
+
+ArrayList.prototype.prepend = function(value) {
+  this._expand();
+  this._shiftRight();
+  this._array.set(0, value);
+  this._size++;
+  return this;
+}
+
+ArrayList.prototype._shiftRight = function() {
+  for (let i = this._size - 1; i >= 0; i--) {
+    this._array.set(i + 1, this._array.get(i));
+  }
+}
+
+ArrayList.prototype._expand = function() {
+  if (this._size != this._array.size()) {
+    return;
+  }
+
+  let tempArray = new FixedArray(this._size * 2);
+
+  for (let i = 0; i < this._size; i++) {
+    tempArray.set(i, this._array.get(i));
+  }
+
+  this._array = tempArray;
+}
 
 // Define a "delete" method which takes a single index argument. This method
 // should delete the value at the provided index and return it. The size should
